2563b3ceafc25dc17a1524393f3e42473288ea83
Awino2000 <awinoemanuel2000@gmail.com>
2022-11-29 12:46:34 +0300

    Initial commit

---
+++ c++ programs/Hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    std::cout << "Hello useless" << std::endl;
+    std::cout << "Hello jinag" << std::endl;
+   //cout << "You uselees nigaa";
+   cout << "Hello kiongos, I hoope you are doing great"<<endl;
+}
